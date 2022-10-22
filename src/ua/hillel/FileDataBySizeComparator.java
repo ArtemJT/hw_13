a0bc35d96b05b768dc29b0ec51a310d36c3d51e7
Artem Kovalov <artemkov1990@gmail.com>
2022-10-22 10:47:37 +0300

    File renamed

--- src/ua/hillel/FileDataBySizeComparator.java
+++ src/ua/hillel/FileDataBySizeComparator.java
@@ -2,7 +2,7 @@ package ua.hillel;
 
 import java.util.Comparator;
 
-public class MyComparator implements Comparator<FileData> {
+public class FileDataBySizeComparator implements Comparator<FileData> {
     @Override
     public int compare(FileData o1, FileData o2) {
         return Long.compare(o1.getFileSize(), o2.getFileSize());
